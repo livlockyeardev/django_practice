25e547590efa40b4d08d0410fdfcb174370679de
livlockyeardev <livlockyear@gmail.com>
2026-01-28 16:07:23 +0000

    add functionality for about app

--- hello_world/tests.py
+++ hello_world/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+def index(request):
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
